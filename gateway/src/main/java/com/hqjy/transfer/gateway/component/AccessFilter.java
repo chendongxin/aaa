7dd4752c7abacae9b223d6109b786762e9c514bc
hejinyo <hejinyo@gmail.com>
2018-09-05 11:34:39 +0800

    改为SpringCloud

---
+++ gateway/src/main/java/com/hqjy/transfer/gateway/component/AccessFilter.java
@@ -0,0 +1,50 @@
+package com.hqjy.transfer.gateway.component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 请求过滤器
+ * @author : HejinYo   hejinyo@gmail.com
+ * @date :  2018/8/18 16:45
+ */
+@Slf4j
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            log.warn("accessToken is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("accessToken ok");
+        return null;
+    }
+}
---
+++ modules/transfer-crm-allot/src/main/java/com/hqjy/transfer/allot/AllotApplication.java
@@ -0,0 +1,12 @@
+package com.hqjy.transfer.allot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllotApplication.class, args);
+    }
+}
