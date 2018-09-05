05a879c6861ff8e0676f4019a39410c2f0f6cb5c
hejinyo <hejinyo@gmail.com>
2018-09-05 12:00:15 +0800

    update

--- modules/transfer-crm-allot/src/main/java/com/hqjy/transfer/allot/AllotApplication.java
+++ modules/transfer-crm-allot/src/main/java/com/hqjy/transfer/allot/AllotApplication.java
@@ -2,10 +2,14 @@ package com.hqjy.transfer.allot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AllotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AllotApplication.class, args);
     }
