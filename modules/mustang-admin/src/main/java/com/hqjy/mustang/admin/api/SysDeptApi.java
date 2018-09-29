a7b239c0f00b75c5f7af9534d9d5727fa4e18cfa
xieyuqing <xieyuqing@hengqijy.com>
2018-09-29 18:07:57 +0800

    合并冲突处理

--- modules/mustang-admin/src/main/java/com/hqjy/mustang/admin/api/SysDeptApi.java
+++ modules/mustang-admin/src/main/java/com/hqjy/mustang/admin/api/SysDeptApi.java
@@ -3,12 +3,16 @@ package com.hqjy.mustang.admin.api;
 import com.hqjy.mustang.admin.service.SysDeptService;
 import com.hqjy.mustang.common.base.constant.Constant;
 import com.hqjy.mustang.common.base.utils.PojoConvertUtil;
+import com.hqjy.mustang.common.base.utils.R;
 import com.hqjy.mustang.common.model.admin.SysDeptInfo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 部门信息
@@ -34,14 +38,13 @@ public class SysDeptApi {
     /**
      * 获取所有部门ID
      */
-    @ApiOperation(value="获取所选部门的旗下部门", notes = "获取所选部门的旗下部门")
+    @ApiOperation(value = "获取所选部门的旗下部门", notes = "获取所选部门的旗下部门")
     @GetMapping(value = "/dept/all/id")
     public List<Long> getAllDeptId(Long deptId) {
         return sysDeptService.getAllDeptUnderDeptId(deptId);
     }
 
-}
-
+    /**
      * 根据部门Id查询
      */
     @GetMapping("/{deptId}")
